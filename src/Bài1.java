f7d39b31b0fc445e82acbd600e581b21f1cd0452
Tuemin <ngaanh2.2510@gmail.com>
2023-09-05 20:37:18 +0700

    BBai1

---
+++ src/Bài1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Bài1 {
+    public static void main(String[] args) {
+        //Scanner sc = new Scanner(System.in);
+        System.out.println("Họ tên: Bùi Thị Nga");
+        System.out.println("Ngày tháng năm sinh: 25/10/1997");
+        System.out.println("Chuyên ngành: Điện tử viễn thông");
+        System.out.println("Mục tiêu học auto test: Cải thiện thu nhập");
+    }
+}

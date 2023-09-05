8c94dfb1467aea039a19b5cf2a9ae822c1a7a816
Tuemin <ngaanh2.2510@gmail.com>
2023-09-05 21:06:21 +0700

    Bai2

---
+++ src/Bai2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Bai2 {
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap vao N = ");
+        n = sc.nextInt();
+        if (n < 2) {
+            System.out.println("Nhap n thoa man 2<=N<=20");
+        } else if (n > 20) {
+            System.out.println("Nhap n thoa man 2<=N<=20");
+        } else {
+            for (int i = 1; i <= 10; i++) {
+                int b;
+                b = n * i;
+                System.out.println(b);
+            }
+        }
+    }
+}
